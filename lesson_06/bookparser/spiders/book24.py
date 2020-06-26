9baf958eb15517e792afed3e6b4064bb738a820e
Mikhail Sokolenko <misha.classic@yandex.ru>
2020-06-26 03:10:45 +0300

    подчищены пауки

--- lesson_06/bookparser/spiders/book24.py
+++ lesson_06/bookparser/spiders/book24.py
@@ -2,20 +2,11 @@
 import scrapy
 from bookparser.items import BookparserItem
 
-# //a[@class='catalog-pagination__item _text js-pagination-catalog-item']
-# //a[@class='book__title-link js-item-element ddl_product_link ']
-
-# //h1[@class='item-detail__title']
-# //span[@class='rating__rate-value']
-
-# //div[@class='item-actions__prices']
-# //div[@class='item-actions__price']
-# //div[@class='item-actions__price-old']
-
 
 class Book24Spider(scrapy.Spider):
     name = 'book24'
     allowed_domains = ['book24.ru']
+    # поисковый запрос - программирование
     start_urls = ['https://book24.ru/search/?q=%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5']
 
     def parse(self, response):
--- lesson_06/bookparser/spiders/labirint.py
+++ lesson_06/bookparser/spiders/labirint.py
@@ -6,6 +6,7 @@ from bookparser.items import BookparserItem
 class LabirintSpider(scrapy.Spider):
     name = 'labirint'
     allowed_domains = ['labirint.ru']
+    # Поисковый запрос - программирование
     start_urls = ['https://www.labirint.ru/search/%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/?available=1&wait=1&preorder=1&paperbooks=1&ebooks=1']
 
     def parse(self, response):
